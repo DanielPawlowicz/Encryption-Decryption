3f17f72669c8dbab03db70ebddad531da9def8fc
Daniel Pawłowicz <danielpawlowicz@interia.pl>
2024-02-01 10:25:23 +0100

    Declaring variables and objects

--- src/Encryption.java
+++ src/Encryption.java
@@ -1,5 +1,17 @@
+import java.util.ArrayList;
+import java.util.Random;
+import java.util.Scanner;
 
 public class Encryption {
+	
+	private Scanner scanner;
+	private Random random;
+	private ArrayList<Character> list;
+	private ArrayList<Character> shuffledList;
+	private char character;
+	private String line;
+	private char[] letters;
+	private char[] secretLetters;
 	
 	Encryption(){
 		
